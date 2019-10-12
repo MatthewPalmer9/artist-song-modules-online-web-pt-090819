fdd37544135c0f12724b096c00454dc9f6f6c08b
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-12 04:07:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 require 'pry'
 require_relative '../lib/concerns/memorable.rb'
 require_relative '../lib/concerns/findable.rb'
+require_relative '../lib/concerns/paramable.rb'
 
 class Song
   extend Memorable
   extend Findable
+  extend Paramable
 
   attr_accessor :name
   attr_reader :artist
