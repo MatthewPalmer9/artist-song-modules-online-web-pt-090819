e9893003005ebe97eb5e01b9e0f8c1374aea7c33
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-12 04:00:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 require 'pry'
-require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/memorable.rb'
 
 class Artist
   extend Memorable
@@ -14,9 +14,9 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+#  def self.find_by_name(name)
+#    @@artists.detect{|a| a.name == name}
+#  end
 
   def self.all
     @@artists
